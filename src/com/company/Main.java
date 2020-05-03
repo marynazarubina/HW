647d26f98bcd017c82f67efa2e5f8f42ff2a39b8
marynazarubina <marichka.zar@gmail.com>
2020-05-03 10:25:38 +0300

    Hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter an integer:");
+        int number = sc.nextInt();
+
+        if (number % 3 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
